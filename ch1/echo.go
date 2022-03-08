3d084dd0d4e17f4d6798974c792aaa44740474d0
rayyu999 <411205547@qq.com>
2022-03-08 12:27:52 +0800

    primer

---
+++ ch1/echo.go
@@ -0,0 +1,36 @@
+// Echo prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	echo1()
+	echo2()
+	echo3()
+}
+
+func echo1() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func echo2() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func echo3() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
